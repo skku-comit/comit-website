20a2882b3a9e2342470538a4376ac5b57fc58840
Taegon <taegon@cusee.net>
2024-02-21 21:31:58 +0900

    header design

--- app/(main)/_components/Header.tsx
+++ app/(main)/_components/Header.tsx
@@ -1,17 +1,47 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import ComitLogo from '@/public/comit.png'
+import { Button } from '@/components/ui/button'
 
 export default function Header() {
+  const buttonStyle = {
+    backgroundColor: 'purple',
+    color: 'white'
+  }
+
   return (
-    <header className="p-4">
-      <nav className="flex items-center gap-10 text-2xl">
+    <header className="absolute h-[80px] w-full">
+      <nav className="flex items-center justify-around">
         <Link href="/">
-          <Image src={ComitLogo} alt="comit" width={60} />
+          <div className=" flex">
+            <Image
+              src={ComitLogo}
+              alt="comit logo"
+              width={29}
+              height={34}
+            ></Image>
+            <div className="flex items-center">
+              <p className="ml-2 font-semibold">CoMit</p>
+            </div>
+          </div>
         </Link>
-        <Link href="/about">About</Link>
-        <Link href="/study">Study</Link>
-        <Link href="/room">Room</Link>
+        <nav className="flex w-[21dvw] justify-between">
+          <Link href="/about" className="flex items-center">
+            <p className="font-medium">About</p>
+          </Link>
+          <Link href="/study" className="flex items-center">
+            <p className="font-medium">Study</p>
+          </Link>
+          <Link href="/clubroom" className="flex items-center">
+            <p className="font-medium">Clubroom</p>
+          </Link>
+        </nav>
+        <div className="flex h-[40px] w-[15vw] justify-between">
+          <Button className="h-[30px] w-[140px]">Log in</Button>
+          <Button className="h-[30px] w-[140px]" variant={'outline'}>
+            Sign up
+          </Button>
+        </div>
       </nav>
     </header>
   )
