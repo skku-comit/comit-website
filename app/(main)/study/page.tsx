5eace8967dc7f63d73f83167d124ac2942d3ba95
b0xercat <seojin3154@naver.com>
2024-02-27 11:12:55 +0900

    feat(fe): add study card

--- app/(main)/study/page.tsx
+++ app/(main)/study/page.tsx
@@ -1,7 +1,65 @@
 import SectionBanner from '@/components/common/SectionBanner'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import StudyCard from '@/components/study/StudyCard'
 
+const studyDummyData = [
+  {
+    imageSrc: 'https://i.esdrop.com/d/f/rWQIyJ1wtg/Z2bQcFPLxZ.svg',
+    title: '모바일 앱 프로젝트',
+    description: '목 19:30 ~ 21:30',
+    badges: ['초급', 'Flutter', '율전']
+  },
+  {
+    imageSrc: 'https://i.esdrop.com/d/f/rWQIyJ1wtg/Z2bQcFPLxZ.svg',
+    title: '모바일 앱 프로젝트',
+    description: '목 19:30 ~ 21:30',
+    badges: ['초급', 'Flutter', '율전']
+  },
+  {
+    imageSrc: 'https://i.esdrop.com/d/f/rWQIyJ1wtg/Z2bQcFPLxZ.svg',
+    title: '모바일 앱 프로젝트',
+    description: '목 19:30 ~ 21:30',
+    badges: ['초급', 'Flutter', '율전']
+  },
+  {
+    imageSrc: 'https://i.esdrop.com/d/f/rWQIyJ1wtg/Z2bQcFPLxZ.svg',
+    title: '모바일 앱 프로젝트',
+    description: '목 19:30 ~ 21:30',
+    badges: ['초급', 'Flutter', '율전']
+  },
+  {
+    imageSrc: 'https://i.esdrop.com/d/f/rWQIyJ1wtg/Z2bQcFPLxZ.svg',
+    title: '모바일 앱 프로젝트',
+    description: '목 19:30 ~ 21:30',
+    badges: ['초급', 'Flutter', '율전']
+  },
+  {
+    imageSrc: 'https://i.esdrop.com/d/f/rWQIyJ1wtg/Z2bQcFPLxZ.svg',
+    title: '모바일 앱 프로젝트',
+    description: '목 19:30 ~ 21:30',
+    badges: ['초급', 'Flutter', '율전']
+  },
+  {
+    imageSrc: 'https://i.esdrop.com/d/f/rWQIyJ1wtg/Z2bQcFPLxZ.svg',
+    title: '모바일 앱 프로젝트',
+    description: '목 19:30 ~ 21:30',
+    badges: ['초급', 'Flutter', '율전']
+  },
+  {
+    imageSrc: 'https://i.esdrop.com/d/f/rWQIyJ1wtg/Z2bQcFPLxZ.svg',
+    title: '모바일 앱 프로젝트',
+    description: '목 19:30 ~ 21:30',
+    badges: [
+      '초급',
+      'Flutter',
+      '율전',
+      'Firebase',
+      '오프라인',
+      '짱 긴 text 입니다'
+    ]
+  }
+]
 export default function Study() {
   return (
     <>
@@ -9,6 +67,11 @@ export default function Study() {
         title="Study"
         description="CoMit의 다양한 스터디 분반을 확인해보세요!"
       />
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4 mb-8">
+        {studyDummyData.map((study, index) => (
+          <StudyCard key={index} {...study} />
+        ))}
+      </div>
       <Button asChild>
         <Link href="study/open">스터디 개설</Link>
       </Button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,7 +29,7 @@ const config = {
           foreground: 'hsl(var(--primary-foreground))'
         },
         secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
+          DEFAULT: '#E4E4E4',
           foreground: 'hsl(var(--secondary-foreground))'
         },
         destructive: {
