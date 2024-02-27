df3fd56ba62e0d596e2c691e215e4b1c26b80292
Taegon <taegon@cusee.net>
2024-02-27 11:23:32 +0900

    add carousel and study

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,6 +2,31 @@ import mainPicture from '@/public/mainPicture.svg'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 
+import { SlArrowLeft } from 'react-icons/sl'
+import { SlArrowRight } from 'react-icons/sl'
+
+import {
+  Carousel,
+  CarouselItem,
+  CarouselContent,
+  CarouselNext,
+  CarouselPrevious
+} from '@/components/ui/carousel'
+const dummyStackUrl: string[] = [
+  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg'
+]
+
 export default function Home() {
   return (
     <div className="min-h-screen w-full bg-black text-center text-white">
@@ -39,7 +64,7 @@ export default function Home() {
       </div>
       <div className="mt-[26.4%] flex justify-center">
         <div className="w-[60%]">
-          <p className="items-start text-[70px] font-semibold">About</p>
+          <p className="text-left text-[70px] font-semibold">About</p>
           <div className="flex justify-between font-semibold">
             <div>
               <p className="text-[40px]">누적 스터디 개설</p>
@@ -53,6 +78,41 @@ export default function Home() {
               <p className="text-[40px]">평균 스터디 개설</p>
               <p className="text-[90px]">15+</p>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-[23%] flex justify-center">
+        <div className="w-[65%] font-semibold">
+          <p className="text-left text-[70px]">Study</p>
+          <p className="mt-[78px] items-start text-left text-[40px]">
+            다뤄진 기술 스택
+          </p>
+          <div className="flex h-[128px] w-full items-center">
+            <Carousel
+              className="flex w-full items-center justify-between"
+              opts={{ align: 'start' }}
+            >
+              <CarouselPrevious />
+              <CarouselContent>
+                {dummyStackUrl.map((item, index) => {
+                  return (
+                    <CarouselItem key={index} className="basis-1/8">
+                      <div>
+                        <Image
+                          width={128}
+                          height={128}
+                          src={item}
+                          unoptimized
+                          alt="stack"
+                          key={index}
+                        ></Image>
+                      </div>
+                    </CarouselItem>
+                  )
+                })}
+              </CarouselContent>
+              <CarouselNext />
+            </Carousel>
           </div>
         </div>
       </div>
