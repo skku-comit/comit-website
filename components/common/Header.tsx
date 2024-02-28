1d257557d0e6f0c5adb1e05f0df0dc73f7108666
b0xercat <seojin3154@naver.com>
2024-02-29 00:22:41 +0900

    fix: edit header responsive

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import ComitLogo from '@/public/comit.png'
 import { Button } from '@/components/ui/button'
 import { usePathname } from 'next/navigation'
+import { GiHamburgerMenu } from 'react-icons/gi'
 import { cn } from '@/lib/utils'
 
 export function NavLink({ href }: { href: string }) {
@@ -25,13 +26,13 @@ export default function Header() {
   return (
     <header
       className={cn(
-        'fixed top-0 z-10 h-20 w-full',
+        'fixed top-0 z-10 flex h-20 w-full justify-center',
         ['/', '/signup', '/login'].includes(pathname)
           ? 'bg-black'
           : 'border-b border-b-gray-200 bg-white'
       )}
     >
-      <nav className="flex h-full items-center justify-around">
+      <nav className="flex h-full w-[1280px] items-center justify-between px-4">
         <Link href="/">
           <div className="flex items-center gap-2">
             <Image src={ComitLogo} alt="comit_logo" width={32} height={37} />
@@ -45,18 +46,18 @@ export default function Header() {
             </p>
           </div>
         </Link>
-        <div className="flex w-[21dvw] justify-between">
+        <div className="hidden md:flex md:gap-10 lg:gap-24">
           <NavLink href="about" />
           <NavLink href="study" />
           <NavLink href="clubroom" />
         </div>
-        <div className="flex h-[40px] w-[310px] items-center justify-between">
-          <Button className="h-[36px] w-[140px] text-base" asChild>
+        <div className="hidden h-[40px] w-[270px] items-center justify-between md:flex lg:w-[310px]">
+          <Button className="h-[36px] w-[120px] text-base lg:w-[140px]" asChild>
             <Link href="/signup">Sign up</Link>
           </Button>
           <Button
             className={cn(
-              'h-[36px] w-[140px] text-base',
+              'h-[36px] w-[120px] text-base lg:w-[140px]',
               ['/', '/signup', '/login'].includes(pathname) && 'border-white'
             )}
             variant="outline"
@@ -65,6 +66,7 @@ export default function Header() {
             <Link href="/login">Log in</Link>
           </Button>
         </div>
+        <GiHamburgerMenu className="md:hidden" size={32}/>
       </nav>
     </header>
   )
