2ebc5717750ccfc9586af0e6d5dd0fd05a5dcce9
b0xercat <seojin3154@naver.com>
2024-02-29 01:00:11 +0900

    feat(fe): add hamburger and drawer

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -3,9 +3,20 @@ import Link from 'next/link'
 import Image from 'next/image'
 import ComitLogo from '@/public/comit.png'
 import { Button } from '@/components/ui/button'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { cn } from '@/lib/utils'
+import {
+  Drawer,
+  DrawerContent,
+  DrawerTrigger,
+  DrawerClose
+} from '@/components/ui/drawer'
+import { IoMdInformationCircleOutline } from 'react-icons/io'
+import { FaLaptopCode } from 'react-icons/fa'
+import { BsDoorOpen } from 'react-icons/bs'
+import { MdLogin } from 'react-icons/md'
+import { PiNotePencilLight } from 'react-icons/pi'
 
 export function NavLink({ href }: { href: string }) {
   const pathname = usePathname()
@@ -23,16 +34,17 @@ export function NavLink({ href }: { href: string }) {
 }
 export default function Header() {
   const pathname = usePathname()
+  const router = useRouter()
   return (
     <header
       className={cn(
-        'fixed top-0 z-10 flex h-20 w-full justify-center',
+        'fixed top-0 z-10 flex h-20 w-full justify-center px-4',
         ['/', '/signup', '/login'].includes(pathname)
           ? 'bg-black'
           : 'border-b border-b-gray-200 bg-white'
       )}
     >
-      <nav className="flex h-full w-[1280px] items-center justify-between px-4">
+      <nav className="flex h-full w-[1280px] items-center justify-between">
         <Link href="/">
           <div className="flex items-center gap-2">
             <Image src={ComitLogo} alt="comit_logo" width={32} height={37} />
@@ -66,8 +78,75 @@ export default function Header() {
             <Link href="/login">Log in</Link>
           </Button>
         </div>
-        <GiHamburgerMenu className="md:hidden" size={32}/>
       </nav>
+      <Drawer className="md:hidden">
+        <DrawerTrigger>
+          <GiHamburgerMenu
+            className={cn(
+              'md:hidden',
+              ['/', '/signup', '/login'].includes(pathname) && 'text-white'
+            )}
+            size={32}
+          />
+        </DrawerTrigger>
+        <DrawerContent className="flex flex-col gap-4 px-4 pb-6">
+          <DrawerClose>
+            <div
+              className="flex items-center gap-4 text-3xl font-medium"
+              onClick={() => {
+                router.push('/about')
+              }}
+            >
+              <IoMdInformationCircleOutline />
+              <p className="flex items-center text-xl">About</p>
+            </div>
+          </DrawerClose>
+          <DrawerClose>
+            <div
+              className="flex items-center gap-4 text-3xl font-medium"
+              onClick={() => {
+                router.push('/study')
+              }}
+            >
+              <FaLaptopCode />
+              <p className="flex items-center text-xl">Study</p>
+            </div>
+          </DrawerClose>
+          <DrawerClose>
+            <div
+              className="flex items-center gap-4 text-3xl font-medium"
+              onClick={() => {
+                router.push('/clubroom')
+              }}
+            >
+              <BsDoorOpen />
+              <p className="flex items-center text-xl">Clubroom</p>
+            </div>
+          </DrawerClose>
+          <DrawerClose>
+            <div
+              className="flex items-center gap-4 text-3xl font-medium"
+              onClick={() => {
+                router.push('/login')
+              }}
+            >
+              <MdLogin />
+              <p className="flex items-center text-xl">Log in</p>
+            </div>
+          </DrawerClose>
+          <DrawerClose>
+            <div
+              className="flex items-center gap-4 text-3xl font-medium"
+              onClick={() => {
+                router.push('/signup')
+              }}
+            >
+              <PiNotePencilLight />
+              <p className="flex items-center text-xl">Sign up</p>
+            </div>
+          </DrawerClose>
+        </DrawerContent>
+      </Drawer>
     </header>
   )
 }
