138c4d9b68363fd31fb386dda1c94afb1a29a6bc
Taegon <taegon@cusee.net>
2024-03-02 17:16:23 +0900

    put stack dummy data

--- lib/dummy.ts
+++ lib/dummy.ts
@@ -7,7 +7,7 @@ export const studyDummyData = [
     startTime: '19:30',
     endTime: '21:30',
     level: '초급',
-    stack: ['Flutter', 'Firebase', 'Dart', "Provider"],
+    stack: ['Flutter', 'Firebase', 'Dart', 'Provider'],
     campus: '율전',
     description:
       '입문자를 위한 flutter 초급 스터디입니다.\n입문자를 위한 flutter 초급 스터디입니다.\n입문자를 위한 flutter 초급 스터디입니다.\n입문자를 위한 flutter 초급 스터디입니다.\n입문자를 위한 flutter 초급 스터디입니다.\n'
@@ -128,22 +128,21 @@ export const studyDummyData = [
     campus: '율전',
     description:
       '입문자를 위한 flutter 초급 스터디입니다.\n입문자를 위한 flutter 초급 스터디입니다.\n입문자를 위한 flutter 초급 스터디입니다.\n입문자를 위한 flutter 초급 스터디입니다.\n입문자를 위한 flutter 초급 스터디입니다.\n'
-  },
+  }
 ]
 
 export const dummyStackUrl: string[] = [
   'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
-  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
-  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
-  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
-  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
-  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
-  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
-  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
-  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
-  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
-  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
-  'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg'
+  'https://i.namu.wiki/i/KcqDuQYTxNpUcLIMZTg28QXse0XiWx1G7K68kYYCo1GuhoHmhB_V8Qe9odGGt0BH9-0nQZTN53WXTNpDmwVfWQ.svg',
+  'https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi8iA3ERV3WZ-Y_OFuQJ80Pz1tLJJkWNW7nYGsP_Vlkw&s',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZeab83jLxJNlqggbc0T40gUZ9D2VGLUg6cI2BNJ6Fwg&s',
+  'https://i.namu.wiki/i/Rv7cLGvX03Y-IX85VC6HXqtKuAhofMYJdodeW2v38Ghm6eCgDCqAhjXWcAWb0MB5UdvweeYI8QLNalwMevPplw.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/2560px-Node.js_logo.svg.png',
+  'https://global.discourse-cdn.com/auth0/optimized/2X/a/ae35edce19e64c53e5d455b22e8a2c82d093d4c9_2_1024x919.png',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/512px-Typescript_logo_2020.svg.png?20221110153201',
+  'https://ih1.redbubble.net/image.3917587429.0371/st,small,845x845-pad,1000x1000,f8f8f8.jpg'
 ]
 
 export const dummyPeopleData = [
