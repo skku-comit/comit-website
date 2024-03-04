351d213a7a2f0bc2e90c86999b2c3c4a55a651bd
b0xercat <seojin3154@naver.com>
2024-03-04 16:40:44 +0900

    chore: add todo comments

--- app/admin/studies/opened/page.tsx
+++ app/admin/studies/opened/page.tsx
@@ -5,12 +5,12 @@ import { adminStudyDummyData } from '@/lib/dummy'
 
 export default function Opened() {
   return (
+    // TODO: 더미데이터 대신 study api를 fetch해서 opened만 filtering 해서 사용
     <div className="flex flex-col gap-8">
       <div className="flex items-center justify-between">
         <div className="text-5xl font-semibold">Opened Studies</div>
         <Button>Close All</Button>
       </div>
-
       <DataTable columns={columns} data={adminStudyDummyData} />
     </div>
   )
--- app/admin/studies/reviewing/page.tsx
+++ app/admin/studies/reviewing/page.tsx
@@ -5,12 +5,12 @@ import { adminStudyDummyData } from '@/lib/dummy'
 
 export default function Reviewing() {
   return (
+    // TODO: 더미데이터 대신 study api를 fetch해서 reviewing만 filtering 해서 사용
     <div className="flex flex-col gap-8">
       <div className="flex items-center justify-between">
         <div className="text-5xl font-semibold">Reviewing Studies</div>
         <Button>Approve All</Button>
       </div>
-
       <DataTable columns={columns} data={adminStudyDummyData} />
     </div>
   )
