124cac58fdcb2fca23910e16e38387e6b92445ea
b0xercat <seojin3154@naver.com>
2024-03-14 16:37:26 +0900

    chore: edit introduce member card css

--- components/about/IntroduceMemberCard.tsx
+++ components/about/IntroduceMemberCard.tsx
@@ -23,12 +23,12 @@ export default function IntroduceMemeberCard({
           className="h-full w-full object-cover"
         />
       </div>
-      <div className="flex h-36 w-80 rounded-3xl border p-8 shadow-md sm:h-44 sm:w-96">
+      <div className="h-36 w-80 rounded-3xl border p-5 shadow-md sm:h-44 sm:w-96 sm:p-7">
         <div className="flex flex-col gap-1 sm:gap-2">
           <p className="text-xl font-semibold sm:text-2xl">
             {name} / {position}
           </p>
-          <p className="w-full text-lg font-medium sm:text-[20px]">
+          <p className="w-full text-base font-medium sm:text-lg">
             {description}
           </p>
         </div>
