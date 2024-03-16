b2ef0cd8c6a77307b5cd0426f5fb783df7d8f397
b0xercat <seojin3154@naver.com>
2024-03-16 20:01:36 +0900

    feat(fe): add isRecruiting field to study schema

--- lib/dummy.ts
+++ lib/dummy.ts
@@ -11,7 +11,8 @@ export const studyDummyData = [
     stack: ['HTML', 'CSS', 'JavaScript'],
     campus: '공통',
     description:
-      'Udemy의 Web 부트캠프 강의를 기반으로 초급 html/css부터 javascript 문법 공부를 통해 자신만의 랜딩 페이지를 만드는 것이 목표입니다. 웹개발에 관심이 있으며 처음 접해보는 분들에게 권장됩니다.'
+      'Udemy의 Web 부트캠프 강의를 기반으로 초급 html/css부터 javascript 문법 공부를 통해 자신만의 랜딩 페이지를 만드는 것이 목표입니다. 웹개발에 관심이 있으며 처음 접해보는 분들에게 권장됩니다.',
+    isRecruiting: true
   },
   {
     imageSrc:
@@ -25,7 +26,8 @@ export const studyDummyData = [
     stack: ['python', 'Apache'],
     campus: '공통',
     description:
-      'Udemy의 파이썬 데이터분석 부트캠프 강의를 기반으로 기본 파이썬 문법을 정리해보고, 다양한 데이터분석 모듈과 기술의 사용법을 공부해 보는 것이 목표입니다. 초반에 파이썬 문법을 정리하는 섹션이 있지만, 조금은 파이썬 문법에 지식이 있는 분들에게 권장됩니다'
+      'Udemy의 파이썬 데이터분석 부트캠프 강의를 기반으로 기본 파이썬 문법을 정리해보고, 다양한 데이터분석 모듈과 기술의 사용법을 공부해 보는 것이 목표입니다. 초반에 파이썬 문법을 정리하는 섹션이 있지만, 조금은 파이썬 문법에 지식이 있는 분들에게 권장됩니다',
+    isRecruiting: true
   },
   {
     imageSrc:
@@ -39,7 +41,8 @@ export const studyDummyData = [
     stack: ['C'],
     campus: '공통',
     description:
-      'Udemy의 c언어 부트캠프 강의를 기반으로 개발환경 세팅과 c언어의 초급 문법부터 공부하여 학기말에 간단한 자유 프로젝트를 제작하는 것이 목표입니다. 프로그래밍이나 c에 대해 지식이 없는 분들에기 권장드립니다.'
+      'Udemy의 c언어 부트캠프 강의를 기반으로 개발환경 세팅과 c언어의 초급 문법부터 공부하여 학기말에 간단한 자유 프로젝트를 제작하는 것이 목표입니다. 프로그래밍이나 c에 대해 지식이 없는 분들에기 권장드립니다.',
+    isRecruiting: true
   },
   {
     imageSrc:
@@ -53,7 +56,8 @@ export const studyDummyData = [
     stack: ['node.js', 'Express', 'Typescript'],
     campus: '율전',
     description:
-      'Udemy 강의 [NodeJS - The Complete Guide] 를 들으면서 Node, Express, Typescript를 학습하는 스터디입니다. Javascript에 능숙하고 웹 동작 원리에 대한 이해가 있는 분들께 권장드립니다. 세부적인 스터디 진행 방식은 모집 후 토의를 통해 결정할 예정입니다. 강의 런타임이 38.5시간으로 긴 편인데 학기 중에 시간 투자해서 같이 완강까지 달릴 분들 신청해주세요!\n\n<a href="https://www.udemy.com/course/nodejs-the-complete-guide/" target="_blank" style="color: blue">[유데미 강의 링크]</a>'
+      'Udemy 강의 [NodeJS - The Complete Guide] 를 들으면서 Node, Express, Typescript를 학습하는 스터디입니다. Javascript에 능숙하고 웹 동작 원리에 대한 이해가 있는 분들께 권장드립니다. 세부적인 스터디 진행 방식은 모집 후 토의를 통해 결정할 예정입니다. 강의 런타임이 38.5시간으로 긴 편인데 학기 중에 시간 투자해서 같이 완강까지 달릴 분들 신청해주세요!\n\n<a href="https://www.udemy.com/course/nodejs-the-complete-guide/" target="_blank" style="color: blue">[유데미 강의 링크]</a>',
+    isRecruiting: true
   },
   {
     imageSrc:
@@ -67,7 +71,8 @@ export const studyDummyData = [
     stack: ['Unity', 'C#'],
     campus: '율전',
     description:
-      'Udemy의 C#강의와 Unity 2d게임 개발 강의를 기반으로 C#문법을 몇주동안 빠르게 공부한 뒤, 유니티 엔진 공부로 넘어가 학기말에 공동 게임 프로젝트를 하나 만드는 것이 목표입니다. unity엔진이 큰 개발 능력을 요구하지는 않기에 초보거나 게임 개발에 관심있으신분들이라면 환영입니다. 다만 c#을 다루기에 어느정도 c언어를 다룰 수 있는 분들에게 권장됩니다.'
+      'Udemy의 C#강의와 Unity 2d게임 개발 강의를 기반으로 C#문법을 몇주동안 빠르게 공부한 뒤, 유니티 엔진 공부로 넘어가 학기말에 공동 게임 프로젝트를 하나 만드는 것이 목표입니다. unity엔진이 큰 개발 능력을 요구하지는 않기에 초보거나 게임 개발에 관심있으신분들이라면 환영입니다. 다만 c#을 다루기에 어느정도 c언어를 다룰 수 있는 분들에게 권장됩니다.',
+    isRecruiting: true
   },
   {
     imageSrc:
@@ -81,7 +86,8 @@ export const studyDummyData = [
     stack: ['Kotlin', 'Android'],
     campus: '율전',
     description:
-      '공식문서를 기반으로 모두가 스터디 동안 각자 하나의 앱을 만들고 배포하는 것을 목표로 스터디를 진행합니다. 매주마다 배운 내용을 적용하여 progress를 발표하고, 서로 피드백하는 과정이 주가 됩니다. 개발경험이 없어도 괜찮지만, 기본적인 알고리즘, 프론트/백엔드,git 등에 대한 지식은 스스로 학습하며 따라오실 수 있는 분들에게 권장됩니다.'
+      '공식문서를 기반으로 모두가 스터디 동안 각자 하나의 앱을 만들고 배포하는 것을 목표로 스터디를 진행합니다. 매주마다 배운 내용을 적용하여 progress를 발표하고, 서로 피드백하는 과정이 주가 됩니다. 개발경험이 없어도 괜찮지만, 기본적인 알고리즘, 프론트/백엔드,git 등에 대한 지식은 스스로 학습하며 따라오실 수 있는 분들에게 권장됩니다.',
+    isRecruiting: true
   },
   {
     imageSrc:
@@ -95,7 +101,8 @@ export const studyDummyData = [
     stack: ['알고리즘', 'C'],
     campus: '율전',
     description:
-      '백준사이트를 통해 알고리즘 문제풀이 오프라인 스터디를 진행합니다. c언어의 기본문법을 배우신분들을 대상으로 진행하며 제일낮은단계인 새싹문제들과 클래스1단계 문제들부터 진행합니다. 매주 진행 예정이며 시간대및 세부사항은 모집후 정할예정입니다.'
+      '백준사이트를 통해 알고리즘 문제풀이 오프라인 스터디를 진행합니다. c언어의 기본문법을 배우신분들을 대상으로 진행하며 제일낮은단계인 새싹문제들과 클래스1단계 문제들부터 진행합니다. 매주 진행 예정이며 시간대및 세부사항은 모집후 정할예정입니다.',
+    isRecruiting: true
   },
   {
     imageSrc:
@@ -109,7 +116,8 @@ export const studyDummyData = [
     stack: ['spring', 'Git'],
     campus: '율전',
     description:
-      'Udemy 강의 [Spring Boot3 & Spring Framework 6 마스터하기!]를 들으면서 spring의 기초부터 학습할 계획입니다. 추가로 필요한 개념이 있다면 온라인 자료를 활용하여 학습할 계획입니다. 주마다 공부한 내용을 공유하고 Git을 활용하여 작은 웹 사이트를 만드는 프로젝트를 기획 중에 있습니다. (세부 계획은 토의를 통해 결정해요!) / 부가적으로 필요한 JAVA, Git, 백엔드와 프론트엔드 기초 지식 등은 미리 학습되어 있으신 분들이 신청하시면 좋을 것 같습니다 :)\n\n공통 공강시간으로 조정 가능\n\n<a href="https://www.udemy.com/course/spring-boot-and-spring-framework-korean/" target="_blank" style="color: blue">[유데미 강의 링크]</a>'
+      'Udemy 강의 [Spring Boot3 & Spring Framework 6 마스터하기!]를 들으면서 spring의 기초부터 학습할 계획입니다. 추가로 필요한 개념이 있다면 온라인 자료를 활용하여 학습할 계획입니다. 주마다 공부한 내용을 공유하고 Git을 활용하여 작은 웹 사이트를 만드는 프로젝트를 기획 중에 있습니다. (세부 계획은 토의를 통해 결정해요!) / 부가적으로 필요한 JAVA, Git, 백엔드와 프론트엔드 기초 지식 등은 미리 학습되어 있으신 분들이 신청하시면 좋을 것 같습니다 :)\n\n공통 공강시간으로 조정 가능\n\n<a href="https://www.udemy.com/course/spring-boot-and-spring-framework-korean/" target="_blank" style="color: blue">[유데미 강의 링크]</a>',
+    isRecruiting: true
   },
   {
     imageSrc:
@@ -123,7 +131,8 @@ export const studyDummyData = [
     stack: ['알고리즘', 'python'],
     campus: '명륜',
     description:
-      '유데미 "알고리즘 코딩 테스트 입문부터 합격까지" 특강을 같이 들으면서 python 알고리즘 기초를 같이 배우고, 백준 문제도 같이 풀어볼 예정입니다. 수준 및 인원, 여건에 따라 유동적으로 운영할 계획입니다.\n\n<a href="https://url.kr/jodhyx/" target="_blank" style="color: blue">[유데미 강의 링크]</a>'
+      '유데미 "알고리즘 코딩 테스트 입문부터 합격까지" 특강을 같이 들으면서 python 알고리즘 기초를 같이 배우고, 백준 문제도 같이 풀어볼 예정입니다. 수준 및 인원, 여건에 따라 유동적으로 운영할 계획입니다.\n\n<a href="https://url.kr/jodhyx/" target="_blank" style="color: blue">[유데미 강의 링크]</a>',
+    isRecruiting: true
   },
   {
     imageSrc:
@@ -137,7 +146,8 @@ export const studyDummyData = [
     stack: ['node.js', 'JavaScript'],
     campus: '율전',
     description:
-      '여러 온라인 자료를 활용하여 먼저 자바스크립트에 대해 학습하고, 실습합니다. 그 후 node.js의 기초를 학습하고, 간단한 백엔드를 구축해보는 활동을 할 예정입니다. (세부계획과 일정은 모집 후 정할 예정입니다)'
+      '여러 온라인 자료를 활용하여 먼저 자바스크립트에 대해 학습하고, 실습합니다. 그 후 node.js의 기초를 학습하고, 간단한 백엔드를 구축해보는 활동을 할 예정입니다. (세부계획과 일정은 모집 후 정할 예정입니다)',
+    isRecruiting: true
   },
   {
     imageSrc:
@@ -151,7 +161,8 @@ export const studyDummyData = [
     stack: ['React', 'Typescript', 'Tailwind', 'Git'],
     campus: '율전',
     description:
-      '공식문서와 각종 온라인 자료들을 활용하여 React, Typescript, Tailwind를 학습하고 실습합니다. 주마다 Git Issue에 공부해야 할 내용과 실습을 올릴 예정입니다. 오프라인으로 만나서는 서로 코드를 리뷰하고 공부한 내용을 공유합니다. HTML, CSS, JS에 대한 사전지식이 있으면서 Git으로 commit, push 해보신 분들 대상으로 진행합니다.'
+      '공식문서와 각종 온라인 자료들을 활용하여 React, Typescript, Tailwind를 학습하고 실습합니다. 주마다 Git Issue에 공부해야 할 내용과 실습을 올릴 예정입니다. 오프라인으로 만나서는 서로 코드를 리뷰하고 공부한 내용을 공유합니다. HTML, CSS, JS에 대한 사전지식이 있으면서 Git으로 commit, push 해보신 분들 대상으로 진행합니다.',
+    isRecruiting: true
   }
 ]
 
