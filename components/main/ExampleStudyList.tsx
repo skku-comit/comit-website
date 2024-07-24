84b6d54b32e6faf6240fd5b38c3e3b05abe4d999
GalaxyBook2 <manamana32321@gmail.com>
2024-07-25 00:01:03 +0900

    hotfix: restore deleted ExampleStudyList

---
+++ components/main/ExampleStudyList.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import StudyCard from '@/components/common/StudyCard'
+import { Study } from '@/types/Study'
+
+import LoadingSpinner from '../common/LoadingSpinner'
+
+export default function ExampleStudyList() {
+  const [studies, setStudies] = useState<Study[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    const res = fetch('api/studies')
+    res
+      .then((res) => res.json())
+      .then((data) => {
+        setStudies(data)
+        setIsLoading(false)
+      })
+  }, [])
+
+  const exampleStudies = studies.slice(0, 4)
+
+  return (
+    <>
+      {isLoading && <LoadingSpinner />}
+      {!isLoading && (
+        <div className="mt-8 grid grid-cols-2 gap-4 sm:gap-12 xl:mb-32 xl:grid-cols-4">
+          {exampleStudies.map((study, index) => (
+            <StudyCard
+              key={index}
+              study={study}
+              imageSize={176}
+              showStatus={false}
+              imageWrapperClassName="mb-8 mt-4 h-24 w-24 overflow-hidden sm:h-44 sm:w-44"
+            />
+          ))}
+        </div>
+      )}
+    </>
+  )
+}
