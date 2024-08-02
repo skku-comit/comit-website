219d86091911733143d043aa036a10a103a9be20
GalaxyBook2 <manamana32321@gmail.com>
2024-08-02 23:43:27 +0900

    chore: move http related constants into api/utils/httpConsts.ts

--- app/api/errors/index.ts
+++ app/api/errors/index.ts
@@ -1,21 +1,4 @@
-/**
- * Whenever someone needs to reference new status code,
- * please add here
- * https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
- */
-export enum HttpStatusCode {
-  OK = 200,
-  Created = 201,
-  NoContent = 204,
-  BadRequest = 400,
-  UnAuthorized = 401,
-  Forbidden = 403,
-  NotFound = 404,
-  MethodNotAllowed = 405,
-  IamAteapot = 418,
-  InternalServerError = 500,
-  NotImplemented = 501
-}
+import { HttpStatusCode } from '../utils/httpConsts'
 
 /**
  * This class standardizes the error object returned from server
---
+++ app/api/utils/httpConsts.ts
@@ -0,0 +1,24 @@
+/**
+ * https://developer.mozilla.org/ko/docs/Web/HTTP/Methods
+ */
+export type HttpMethod = 'CREATE' | 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
+
+/**
+ * Whenever someone needs to reference new status code,
+ * please add here
+ * https://developer.mozilla.org/ko/docs/Web/HTTP/Status
+ */
+export enum HttpStatusCode {
+  OK = 200,
+  Created = 201,
+  NoContent = 204,
+  SeeOther = 303,
+  BadRequest = 400,
+  UnAuthorized = 401,
+  Forbidden = 403,
+  NotFound = 404,
+  MethodNotAllowed = 405,
+  IamAteapot = 418,
+  InternalServerError = 500,
+  NotImplemented = 501
+}
