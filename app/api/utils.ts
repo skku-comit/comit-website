c704475df9ad50763ac12a7d534a3cd914c3fb9f
GalaxyBook2 <manamana32321@gmail.com>
2024-08-02 15:35:15 +0900

    feat: implement CRUD factory

--- app/api/utils.ts
+++ app/api/utils.ts
@@ -1,58 +1,96 @@
-import { NextResponse } from 'next/server'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { NotImplementedError } from '@/errors'
 import BaseEntity from '@/types'
 
-export function ReturnObjectAsJsonOr404(obj: any): NextResponse {
-  if (obj) {
-    return NextResponse.json(obj)
-  } else {
-    return NextResponse.json({ error: 'Instance not found' }, { status: 404 })
-  }
-}
-
 function CreateFactory<E extends BaseEntity>() {
-  return (request: Request, { params }: { params: { id: string } }) => {
-    throw new NotImplementedError()
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      const entityCreated = req.body
+      res.status(201).json(entityCreated)
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
   }
 }
 
 function RetrieveFactory<E extends BaseEntity>(entities: E[]) {
-  return (request: Request, { params }: { params: { id: string } }) => {
-    return ReturnObjectAsJsonOr404(entities.find((instance) => instance.id === params.id))
+  return (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      const { id } = req.query
+      const entityFound = entities.find((instance) => instance.id === id)
+
+      if (!entityFound) {
+        res.status(404).json({ error: 'Not found' })
+        return
+      }
+      res.status(200).json(entityFound)
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
   }
 }
 
 function ListFactory<E extends BaseEntity>(entities: E[]) {
-  return (request: Request) => {
-    let searchResult = entities.slice()
+  return (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      let searchResult = entities.slice()
+      const url = new URL(req.url!)
 
-    const url = new URL(request.url)
-
-    const queryParams = url.searchParams
-    queryParams.forEach((key, value) => {
-      searchResult = searchResult.filter((entity) => {
-        // Entity가 queryParam을 속성으로 가지면 필터
-        if (Object.hasOwn(entity, value)) {
-          let queryKey = value as keyof E
-          return entity[queryKey] === queryParams.get(value)
-        }
+      const queryParams = url.searchParams
+      queryParams.forEach((key, value) => {
+        searchResult = searchResult.filter((entity) => {
+          // Entity가 queryParam을 속성으로 가지면 필터
+          if (Object.hasOwn(entity, value)) {
+            let queryKey = value as keyof E
+            return entity[queryKey] === key
+          }
+        })
       })
-    })
 
-    return NextResponse.json(searchResult)
+      res.status(200).json(searchResult)
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
   }
 }
 
-function UpdateFactory<E extends BaseEntity>() {
-  return (request: Request, { params }: { params: { id: string } }) => {
-    throw new NotImplementedError()
+function PutFactory<E extends BaseEntity>(entities: E[]) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      const { id } = req.query
+      const entityFound = entities.find((instance) => instance.id === id)
+
+      if (!entityFound) {
+        res.status(404).json({ error: 'Not Found' })
+      }
+
+      const entityUpdated = req.body
+      res.status(201).json(entityUpdated)
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
   }
 }
 
-function DeleteFactory<E extends BaseEntity>() {
-  return (request: Request, { params }: { params: { id: string } }) => {
-    throw new NotImplementedError()
+function PatchFactory<E extends BaseEntity>(entities: E[]) {
+  return PutFactory(entities)
+}
+
+function DeleteFactory<E extends BaseEntity>(entities: E[]) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      const { id } = req.query
+      const entityFound = entities.find((instance) => instance.id === id)
+
+      if (!entityFound) {
+        res.status(404).json({ error: 'Not Found' })
+      }
+
+      const entityUpdated = req.body
+      res.status(204).end()
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
   }
 }
 
@@ -63,7 +101,8 @@ const api = {
   CreateFactory,
   RetrieveFactory,
   ListFactory,
-  UpdateFactory,
+  PutFactory,
+  PatchFactory,
   DeleteFactory
 }
 export { api }
