9cb30ed4e30660c966885a41942f83dcfd4843db
GalaxyBook2 <manamana32321@gmail.com>
2024-08-03 15:36:51 +0900

    fix: fix default scroll position bug

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,12 +3,15 @@ import Header from '@/components/common/Header'
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen w-screen overflow-x-hidden overflow-y-scroll" style={{ WebkitOverflowScrolling: 'touch' }}>
-      <div className="flex min-h-dvh flex-col items-center">
-        <Header />
-        <div className="flex w-full justify-center pt-20">{children}</div>
+    <>
+      <Header />
+      <div
+        className="mt-20 h-screen w-screen overflow-x-hidden overflow-y-scroll"
+        style={{ WebkitOverflowScrolling: 'touch' }}
+      >
+        <div className="flex w-full justify-center">{children}</div>
         <Footer />
       </div>
-    </div>
+    </>
   )
 }
