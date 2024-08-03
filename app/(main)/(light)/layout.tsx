26a1ff7488bb67b6723bbfe2685a69458a76d578
GalaxyBook2 <manamana32321@gmail.com>
2024-08-03 16:12:09 +0900

    refactor: change Header component type from client to server

--- app/(main)/(light)/layout.tsx
+++ app/(main)/(light)/layout.tsx
@@ -1,3 +1,19 @@
+import Footer from '@/components/common/Footer'
+import Header from '@/components/common/Header'
+
 export default function LightLayout({ children }: { children: React.ReactNode }) {
-  return <main className="flex min-h-dvh w-[1280px] flex-1 flex-col items-center">{children}</main>
+  return (
+    <>
+      <Header isDarkMode={false} />
+      <div
+        className="mt-20 h-screen w-screen overflow-x-hidden overflow-y-scroll"
+        style={{ WebkitOverflowScrolling: 'touch' }}
+      >
+        <div className="flex w-full justify-center">
+          <main className="flex min-h-dvh w-[1280px] flex-1 flex-col items-center">{children}</main>
+        </div>
+        <Footer />
+      </div>
+    </>
+  )
 }
