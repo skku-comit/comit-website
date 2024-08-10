fb55d7419ae35032fae76ce6558b1874af63a6fd
GalaxyBook2 <manamana32321@gmail.com>
2024-08-10 20:16:32 +0900

    hotfix: fix api prefix

--- constants/apiEndpoint.ts
+++ constants/apiEndpoint.ts
@@ -1,7 +1,5 @@
 import { HttpMethod } from '@/app/api/utils/httpConsts'
 
-const baseURL = 'http://localhost:3000'
-
 const API_PREFIX = 'api'
 
 export interface ApiEndpoint {
@@ -32,17 +30,17 @@ interface Routes {
 
 export const API_ENDPOINTS: Routes = {
   STUDY: {
-    CREATE: { url: `${baseURL}/${API_PREFIX}/studies`, method: 'POST' },
-    RETRIEVE: (id: string) => ({ url: `${baseURL}/${API_PREFIX}/studies/${id}`, method: 'GET' }),
-    LIST: { url: `${baseURL}/${API_PREFIX}/studies`, method: 'GET' },
-    UPDATE: (id: string) => ({ url: `${baseURL}/${API_PREFIX}/studies/${id}`, method: 'PUT' }),
-    DELETE: (id: string) => ({ url: `${baseURL}/${API_PREFIX}/studies/${id}`, method: 'DELETE' })
+    CREATE: { url: `/${API_PREFIX}/studies`, method: 'POST' },
+    RETRIEVE: (id: string) => ({ url: `/${API_PREFIX}/studies/${id}`, method: 'GET' }),
+    LIST: { url: `/${API_PREFIX}/studies`, method: 'GET' },
+    UPDATE: (id: string) => ({ url: `/${API_PREFIX}/studies/${id}`, method: 'PUT' }),
+    DELETE: (id: string) => ({ url: `/${API_PREFIX}/studies/${id}`, method: 'DELETE' })
   },
   MEMBER: {
-    CREATE: { url: `${baseURL}/${API_PREFIX}/users`, method: 'POST' },
-    RETRIEVE: (id: string) => ({ url: `${baseURL}/${API_PREFIX}/users/${id}`, method: 'GET' }),
-    LIST: { url: `${baseURL}/${API_PREFIX}/users`, method: 'GET' },
-    UPDATE: (id: string) => ({ url: `${baseURL}/${API_PREFIX}/users/${id}`, method: 'PUT' }),
-    DELETE: (id: string) => ({ url: `${baseURL}/${API_PREFIX}/users/${id}`, method: 'DELETE' })
+    CREATE: { url: `/${API_PREFIX}/users`, method: 'POST' },
+    RETRIEVE: (id: string) => ({ url: `/${API_PREFIX}/users/${id}`, method: 'GET' }),
+    LIST: { url: `/${API_PREFIX}/users`, method: 'GET' },
+    UPDATE: (id: string) => ({ url: `/${API_PREFIX}/users/${id}`, method: 'PUT' }),
+    DELETE: (id: string) => ({ url: `/${API_PREFIX}/users/${id}`, method: 'DELETE' })
   }
 }
