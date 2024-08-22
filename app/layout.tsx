96ba092bf2cf382854ddf3d8c909cd78fb84bb8a
GalaxyBook2 <manamana32321@gmail.com>
2024-08-23 07:53:25 +0900

    feat: add HeightPopulatedBody

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,34 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import HeightPopulatedBody from '@/components/common/HeightPopulatedBody'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'COMIT',
   description: 'SKKU COMIT official website',
   icons: {
-    icon: '/favicon.ico'
-  }
+    icon: '/favicon.ico',
+    apple: '/favicon.ico'
+  },
+  openGraph: {
+    title: 'COMIT - SKKU COMIT Official Website',
+    description: 'SKKU COMIT official website',
+    images: [
+      {
+        url: 'https://github.com/skku-comit/comit-website/assets/97675977/deb60102-16d9-41b0-aa16-12d517f20812',
+        width: 1200,
+        height: 630,
+        alt: 'COMIT Banner Image'
+      }
+    ],
+    url: 'https://comit-website.vercel.app/',
+    type: 'website'
+  },
+  viewport: 'width=device-width, initial-scale=1',
+  keywords: ['SKKU', 'COMIT', 'Website', 'Official'],
+  robots: 'index, follow'
 }
 
 export default function RootLayout({
@@ -19,18 +39,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta property="og:url" content="https://comit-website.vercel.app/" />
-        <meta property="og:title" content="COMIT" />
-        <meta property="og:description" content="SKKU COMIT official website" />
-        <meta
-          property="og:image"
-          content="https://github.com/skku-comit/comit-website/assets/97675977/deb60102-16d9-41b0-aa16-12d517f20812"
-        />
-      </head>
-      <body className={inter.className}>{children}</body>
+    <html lang="ko">
+      <HeightPopulatedBody headerHeight="4rem" footerHeight="5rem" className={inter.className}>
+        {children}
+      </HeightPopulatedBody>
     </html>
   )
 }
