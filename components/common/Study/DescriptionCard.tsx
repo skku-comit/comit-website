80f0d9542bf3a56ed72a5c0f3e16d15bc99b910e
b0xercat <seojin3154@naver.com>
2024-08-31 15:20:31 +0900

    feat(fe): change study notice text, edit description card ui

--- components/common/Study/DescriptionCard.tsx
+++ components/common/Study/DescriptionCard.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { FaCheck } from 'react-icons/fa6'
 
 import { Button } from '@/components/ui/button'
 
@@ -16,20 +17,23 @@ export interface DescriptionCardProps {
 
 export default function DescriptionCard({ description, hasButton }: DescriptionCardProps) {
   return (
-    <div className="relative flex w-full max-w-[550px]  flex-col items-start justify-start gap-4 rounded-3xl bg-[#F4F7FA] p-6 lg:w-[45%] xl:p-10">
-      <div className="text-3xl font-bold">{description.title}</div>
-      <div className="text-xl font-semibold">{description.question}</div>
-      <div className="mt-4 text-pretty text-xl font-semibold">{description.recommendation}</div>
+    <div className="relative flex w-full max-w-[550px]  flex-col items-start justify-start gap-4 rounded-3xl bg-[#F4F7FA] p-6 shadow-md lg:w-[45%] xl:p-10">
+      <div className="flex gap-2 text-3xl font-extrabold">
+        <FaCheck className="text-primary" size={32} />
+        {description.title}
+      </div>
+      <div className="text-xl font-semibold text-black">{description.question}</div>
+      <div className="text-pretty text-xl font-semibold text-black">{description.recommendation}</div>
       <ul className="mt-5 list-outside list-disc text-wrap pl-5">
         {description.notices.map((notice) => (
-          <li key={notice} className="mb-3 text-pretty text-sm text-[#6A6D70] md:text-base">
+          <li key={notice} className="mb-3 text-pretty text-sm text-gray-700 md:text-base">
             {notice}
           </li>
         ))}
       </ul>
       {hasButton && (
-        <Button asChild variant="default" className="bottom-2 right-4 self-end font-semibold sm:absolute">
-          <Link href="study/open">스터디 개설신청</Link>
+        <Button asChild variant="outline" className="bottom-4 right-4 self-end font-semibold sm:absolute">
+          <Link href="study/open">스터디 개설</Link>
         </Button>
       )}
     </div>
