744fab46174c41486bfd036b5a90f58f5fb0d8e5
b0xercat <seojin3154@naver.com>
2024-09-03 19:38:26 +0900

    refactor(fe): refactor my study components

--- components/mystudy/MyStudyHeader.tsx
+++ components/mystudy/MyStudyHeader.tsx
@@ -1,13 +1,9 @@
 export default function MyStudyHeader() {
   return (
-    <section id="mystudy-header" className="box-border flex h-20 w-full items-center bg-[#333b3d] py-2 sm:h-28 sm:py-6">
-      <div id="mystudy-header__content" className="mx-auto box-border text-white lg:w-3/5">
-        <h2 id="mystudy-header__title" className="block text-xl font-semibold sm:h-6 sm:text-[28px]">
-          내가 신청하거나 개설한 스터디를 확인해보세요
-        </h2>
-        <p id="mystudy-header__sub-title" className="mt-1 block text-[14px] font-medium sm:mt-4 sm:text-lg">
-          강의 수강에서 더 나아가 함께 스터디까지!
-        </p>
+    <section className="flex h-20 w-full items-center bg-gray-800 py-2 sm:h-28 sm:py-6">
+      <div className="mx-auto flex flex-col gap-1 text-white sm:gap-4 lg:w-3/5">
+        <h2 className="text-lg font-semibold sm:h-6 sm:text-2xl">내가 신청하거나 개설한 스터디를 확인해보세요</h2>
+        <p className="text-sm font-medium sm:text-lg">강의 수강에서 더 나아가 함께 스터디까지!</p>
       </div>
     </section>
   )
--- components/mystudy/MyStudyLeftSide.tsx
+++ components/mystudy/MyStudyLeftSide.tsx
@@ -3,40 +3,45 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
+import { cn } from '@/lib/utils'
+
 export default function MyStudyLeftSide() {
   const pathname = usePathname()
-  const activeStyle = {
-    color: '#9333ea', // purple-600
-    fontWeight: 'bold'
-  }
 
   return (
-    <div id="mystudy-left-side" className="mr-10 mt-11 box-border hidden w-40 sm:block">
-      <nav id="mystudy-body__navigation" className="m-0 box-border p-0">
-        <h6 className="mb-4 box-border h-5 text-[12px] font-medium text-[#adb5bd]">함께 공부해요</h6>
-        <ul id="mystudy-aside" className=" m-0 box-border p-0 text-[17px]">
+    <div className="mr-10 mt-11 hidden w-40 sm:block">
+      <nav className="flex flex-col gap-4">
+        <h6 className="text-sm font-medium text-gray-400">함께 공부해요</h6>
+        <ul>
           <Link href="/mystudy" className="cursor-pointer">
-            <li className="box-border px-3 py-[10px] font-normal" style={pathname === '/mystudy' ? activeStyle : {}}>
+            <li
+              className={cn(
+                'px-3 py-2 font-normal',
+                pathname === '/mystudy' ? 'font-bold text-purple-600' : 'text-gray-600'
+              )}
+            >
               나의 스터디
             </li>
           </Link>
           <Link href="/mystudy/feedback" className="cursor-pointer">
             <li
-              className="box-border px-3 py-[10px] font-normal"
-              style={pathname === '/mystudy/feedback' ? activeStyle : {}}
+              className={cn(
+                'px-3 py-2 font-normal',
+                pathname === '/mystudy/feedback' ? 'font-bold text-purple-600' : 'text-gray-600'
+              )}
             >
               스터디 피드백
             </li>
           </Link>
-
-          <Link
+          {/* TODO: 코밋 스터디 가이드 추가 */}
+          {/* <Link
             href="https://alluring-raccoon-85b.notion.site/1-0-a73896153c484a38a1749dfd76f46094"
             className="cursor-pointer"
           >
-            <li className="box-border px-3 py-[10px] font-normal" style={pathname === '노션링크' ? activeStyle : {}}>
+            <li className="px-3 py-[10px] font-normal" style={pathname === '노션링크' ? activeStyle : {}}>
               스터디 가이드
             </li>
-          </Link>
+          </Link> */}
         </ul>
       </nav>
     </div>
