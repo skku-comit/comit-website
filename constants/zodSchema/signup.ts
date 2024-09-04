c64bcd886c3315c7b68819191479015c45c44458
b0xercat <seojin3154@naver.com>
2024-09-04 16:42:58 +0900

    fix(fe): use extended signup schema for client join

--- constants/zodSchema/signup.ts
+++ constants/zodSchema/signup.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 import { email, password } from '@/constants/zodSchema/signin'
 
-const signUpSchema = z.object({
+export const signUpSchema = z.object({
   username: z.string({ required_error: '필수 입력사항입니다.' }).min(1, '필수 입력사항입니다.'),
   phoneNumber: z
     .string({ required_error: '핸드폰 번호를 입력해주세요.' })
@@ -13,10 +13,11 @@ const signUpSchema = z.object({
     .min(10, '학번은 10자리여야 합니다.')
     .max(10, '학번은 10자리여야 합니다.'),
   email,
-  password,
+  password
+})
+
+export const extendedSignUpSchema = signUpSchema.extend({
   consent: z.literal(true, {
     errorMap: () => ({ message: '개인 정보 수집 / 이용 동의는 필수입니다.' })
   })
 })
-
-export default signUpSchema
