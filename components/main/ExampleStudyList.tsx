97b7b3c640ba153e4d97e0037ca82b94b540f107
GalaxyBook2 <manamana32321@gmail.com>
2024-09-04 15:53:09 +0900

    refactor: add grid to example studies

--- components/main/ExampleStudyList.tsx
+++ components/main/ExampleStudyList.tsx
@@ -22,14 +22,16 @@ export const ExampleStudyList = async (): Promise<React.JSX.Element> => {
   return (
     <div className="mt-8 grid grid-cols-2 gap-4 sm:gap-12 xl:mb-32 xl:grid-cols-4">
       {exampleStudies.map((study) => (
-        <Link key={study.id} href="study/">
-          <StudyCard
-            study={study}
-            imageSize={176}
-            showStatus={false}
-            imageWrapperClassName="mb-8 mt-4 h-24 w-24 overflow-hidden sm:h-44 sm:w-44"
-          />
-        </Link>
+        <div className="col-span-2 flex justify-center sm:col-span-1" key={study.id}>
+          <Link key={study.id} href="study/">
+            <StudyCard
+              study={study}
+              imageSize={176}
+              showStatus={false}
+              imageWrapperClassName="mb-8 mt-4 h-24 w-24 overflow-hidden sm:h-44 sm:w-44"
+            />
+          </Link>
+        </div>
       ))}
     </div>
   )
