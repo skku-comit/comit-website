a0ab7f801c34ab42d2fd93edf542cd78a2c48cb2
b0xercat <seojin3154@naver.com>
2024-09-04 16:07:32 +0900

    fix(fe): add validation for consent

--- constants/zodSchema/signup.ts
+++ constants/zodSchema/signup.ts
@@ -13,7 +13,10 @@ const signUpSchema = z.object({
     .min(10, '학번은 10자리여야 합니다.')
     .max(10, '학번은 10자리여야 합니다.'),
   email,
-  password
+  password,
+  consent: z.literal(true, {
+    errorMap: () => ({ message: '개인 정보 수집 / 이용 동의는 필수입니다.' })
+  })
 })
 
 export default signUpSchema
