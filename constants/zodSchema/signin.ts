a4a8e089da489a77ea84fee4b18fe8e58fb0c47a
b0xercat <seojin3154@naver.com>
2024-09-04 20:22:27 +0900

    hotfix: edit signin schema

--- constants/zodSchema/signin.ts
+++ constants/zodSchema/signin.ts
@@ -6,7 +6,7 @@ export const email = z
   .email('유효하지 않은 이메일입니다.')
 export const password = z
   .string({ required_error: '필수' })
-  .regex(/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,20}$/, '영문, 숫자를 포함한 6~20자리의 비밀번호여야 합니다.')
+  .regex(/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d!@#$%^&*]{6,20}$/, '영문, 숫자를 포함한 6~20자리의 비밀번호여야 합니다.')
 
 const signInSchema = z.object({
   email,
