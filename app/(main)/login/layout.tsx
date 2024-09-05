eec9cc7e2da38f0dfcb45e265f6c7ec52379fa76
GalaxyBook2 <manamana32321@gmail.com>
2024-09-06 04:41:12 +0900

    feat: add reactive session provider

--- app/(main)/login/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import AuthProvider from '@/lib/auth/AuthProvider'
-
-const LoginLayout = ({ children }: { children: React.ReactNode }) => {
-  return <AuthProvider>{children}</AuthProvider>
-}
-
-export default LoginLayout
