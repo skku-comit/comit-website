661f28859267846cdcd021033a252e138b5cb2a7
GalaxyBook2 <manamana32321@gmail.com>
2024-09-06 05:49:17 +0900

    fix: change useState type

--- lib/auth/SessionProvider.tsx
+++ lib/auth/SessionProvider.tsx
@@ -3,25 +3,27 @@ import { usePathname } from 'next/navigation'
 import { getSession } from 'next-auth/react'
 import { createContext, useContext, useEffect, useState } from 'react'
 
-import { AuthData, AuthDataData, AuthDataError } from '@/types/auth'
+import { AuthDataData, AuthDataError } from '@/types/auth'
 
-const SessionContent = createContext<AuthData | null>(null)
+type SuccessAuthData = { error: null; data: AuthDataData }
+type FailAuthData = { error: AuthDataError; data: null }
+
+const SessionContent = createContext<SuccessAuthData | FailAuthData | null>(null)
 
 export const SessionProvider = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname()
-  const [session, setSession] = useState<
-    { error: null; data: AuthDataData } | { error: AuthDataError; data: null } | null
-  >(null) // error, data 둘 중 하나만 존재할 수 있음
+  const [session, setSession] = useState<SuccessAuthData | FailAuthData | null>(null) // error, data 둘 중 하나만 존재할 수 있음
+
   useEffect(() => {
     getSession().then((res) => {
       if (!res) return setSession(null)
       if (res.error) {
-        setSession({ error: res.error, data: null })
+        setSession({ error: res.error, data: null } as FailAuthData)
       } else if (res.data) {
         return setSession({
           error: null,
           data: res.data
-        })
+        } as SuccessAuthData)
       } else {
         setSession(null)
       }
