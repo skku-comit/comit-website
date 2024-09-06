04d0d17d888c11c97e795ee6992aa9a24250fe2a
b0xercat <seojin3154@naver.com>
2024-09-06 10:27:14 +0900

    fix(fe): use column header, adjust order of column

--- components/admin/DataTable/columns/User.tsx
+++ components/admin/DataTable/columns/User.tsx
@@ -3,6 +3,7 @@
 import { ColumnDef } from '@tanstack/react-table'
 
 import EditableCell from '@/components/admin/DataTable/EditableCell'
+import { DataTableColumnHeader } from '@/components/common/DataTable/ColumnHeader'
 import { API_ENDPOINTS } from '@/constants/apiEndpoint'
 import { User } from '@/types'
 
@@ -21,37 +22,28 @@ export const columns: ColumnDef<User>[] = [
   },
   {
     accessorKey: 'username',
-    header: () => <div className="ml-8 min-w-24 text-left text-base">이름</div>,
+    header: ({ column }) => <DataTableColumnHeader column={column} title="이름" />,
     cell: ({ row }) => <p className="ml-8 text-base">{row.original.username}</p>
   },
   {
-    accessorKey: 'bio',
-    header: () => <div className="ml-8 min-w-24 text-left text-base">자기소개</div>,
-    cell: ({ row }) => <p className="ml-8 text-base">{row.original.bio}</p>
+    accessorKey: 'studentId',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="학번" />,
+    cell: ({ row }) => <p className="ml-8 text-base">{row.original.studentId}</p>
   },
   {
-    accessorKey: 'blog',
-    header: () => <div className="ml-8 min-w-24 text-left text-base">블로그</div>,
-    cell: ({ row }) => <p className="ml-8 text-base">{row.original.blog}</p>
-  },
-  {
-    accessorKey: 'createDate',
-    header: () => <div className="mr-8 min-w-56 text-base">생성일</div>,
-    cell: ({ row }) => <p className="mr-8 text-center text-base">{row.original.createdDate}</p>
-  },
-  {
-    accessorKey: 'email',
-    header: () => <div className="ml-8 min-w-24 text-left text-base">이메일</div>,
-    cell: ({ row }) => <p className="ml-8 text-base">{row.original.email}</p>
-  },
-  {
-    accessorKey: 'github',
-    header: () => <div className="ml-8 min-w-24 text-left text-base">깃허브</div>,
-    cell: ({ row }) => <p className="ml-8 text-base">{row.original.github}</p>
+    accessorKey: 'role',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="권한" />,
+    cell: ({ row }) => (
+      <EditableCell
+        submitApiEndpoint={API_ENDPOINTS.ADMIN.USER.ROLE_UPDATE(row.original.id)}
+        row={row}
+        fieldName="role"
+      />
+    )
   },
   {
     accessorKey: 'isStaff',
-    header: () => <div className="ml-8 min-w-24 text-left text-base">스태프 여부</div>,
+    header: ({ column }) => <DataTableColumnHeader column={column} title="임원진 여부" />,
     cell: ({ row }) => (
       <EditableCell
         submitApiEndpoint={API_ENDPOINTS.ADMIN.USER.STAFF_UPDATE(row.original.id)}
@@ -61,18 +53,43 @@ export const columns: ColumnDef<User>[] = [
     )
   },
   {
+    accessorKey: 'bio',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="자기소개" />,
+    cell: ({ row }) => <p className="ml-8 text-base">{row.original.bio}</p>
+  },
+  {
+    accessorKey: 'blog',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="블로그" />,
+    cell: ({ row }) => <p className="ml-8 text-base">{row.original.blog}</p>
+  },
+  {
+    accessorKey: 'createDate',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="생성일" />,
+    cell: ({ row }) => <p className="mr-8 text-center text-base">{row.original.createdDate}</p>
+  },
+  {
+    accessorKey: 'email',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="이메일" />,
+    cell: ({ row }) => <p className="ml-8 text-base">{row.original.email}</p>
+  },
+  {
+    accessorKey: 'github',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="깃허브" />,
+    cell: ({ row }) => <p className="ml-8 text-base">{row.original.github}</p>
+  },
+  {
     accessorKey: 'modifiedDate',
-    header: () => <div className="ml-8 min-w-24 text-left text-base">수정일</div>,
+    header: ({ column }) => <DataTableColumnHeader column={column} title="수정일" />,
     cell: ({ row }) => <p className="ml-8 text-base">{row.original.modifiedDate}</p>
   },
   {
     accessorKey: 'phoneNumber',
-    header: () => <div className="ml-8 min-w-24 text-left text-base">전화번호</div>,
+    header: ({ column }) => <DataTableColumnHeader column={column} title="전화번호" />,
     cell: ({ row }) => <p className="ml-8 text-base">{row.original.phoneNumber}</p>
   },
   {
     accessorKey: 'position',
-    header: () => <div className="ml-8 min-w-24 text-left text-base">직책</div>,
+    header: ({ column }) => <DataTableColumnHeader column={column} title="직책" />,
     cell: ({ row }) => (
       <EditableCell
         submitApiEndpoint={API_ENDPOINTS.ADMIN.USER.POSITION_UPDATE(row.original.id)}
@@ -83,23 +100,7 @@ export const columns: ColumnDef<User>[] = [
   },
   {
     accessorKey: 'profileImage',
-    header: () => <div className="ml-8 min-w-24 text-left text-base">프로필 사진</div>,
-    cell: ({ row }) => <p className="ml-8 text-base">{row.original.profileImage}</p>
-  },
-  {
-    accessorKey: 'role',
-    header: () => <div className="ml-8 min-w-24 text-left text-base">권한</div>,
-    cell: ({ row }) => (
-      <EditableCell
-        submitApiEndpoint={API_ENDPOINTS.ADMIN.USER.ROLE_UPDATE(row.original.id)}
-        row={row}
-        fieldName="role"
-      />
-    )
-  },
-  {
-    accessorKey: 'studentId',
-    header: () => <div className="ml-8 min-w-24 text-left text-base">학번</div>,
-    cell: ({ row }) => <p className="ml-8 text-base">{row.original.studentId}</p>
+    header: ({ column }) => <DataTableColumnHeader column={column} title="프로필 사진" />,
+    cell: ({ row }) => <img src={row.original.profileImage} alt={row.original.username} className="h-10 w-10" />
   }
 ]
